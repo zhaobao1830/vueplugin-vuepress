02a91c7d7a29d18142bcf6cf6e6a6857eb1666c4
zhaobao1830 <zgt..5219438>
2021-01-19 09:12:11 +0800

    基础配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,5 +2,13 @@ const pkg = require('../../package.json')
 
 module.exports = {
   title: pkg.name,
-  description: 'vue插件文档'
+  description: 'vue插件文档',
+  base: '/plugin/',
+  themeConfig: {
+    nav: [
+      { text: '主页', link: '/' },
+      { text: '简介', link: '/guide/introduction' },
+      { text: '常见问题', link: '/problem' }
+    ]
+  }
 }
